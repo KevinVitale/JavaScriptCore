a28bf82f4bc6d5c49cba91687b7cdd60e5805174
Kevin Vitale <kevin.vitale@tdameritrade.com>
2017-04-05 14:40:44 -0400

    [Misc] adds some documentation

--- JavaScriptCore.playground/Sources/JavaScriptCore+Utils.swift
+++ JavaScriptCore.playground/Sources/JavaScriptCore+Utils.swift
@@ -1,6 +1,7 @@
 import JavaScriptCore
 
 /**
+ Extends `JSContext` in a way that reduces boilerplate.
  */
 extension JSContext {
     /**
@@ -11,14 +12,17 @@ extension JSContext {
     }
     
     /**
+     A value which represents a block of JavaScript code.
      */
     public struct Script: ExpressibleByStringLiteral {
+        /**
+         */
         public enum LoadError: Error {
             case notFound(bundle: Bundle)
             case unknown(error: Error?)
         }
         
-        ///
+        /// The underlying String value.
         private let rawValue: String
         
         /**
@@ -40,6 +44,14 @@ extension JSContext {
         }
         
         /**
+         If `self` represents the name of a file, this reads the contents
+         of the file, __iff__ its located in `bundle`.
+         
+         - parameter bundle: The bundle which contains the file.
+         - returns: The contents of the file.
+         
+         - note: If the file cannot be located within `bundle`, a `LoadError`
+                 exception is thrown.
          */
         fileprivate func load(in bundle: Bundle) throws -> String {
             guard let url = bundle.url(forResource: rawValue, withExtension: nil) else {
@@ -49,24 +61,53 @@ extension JSContext {
             return try String(contentsOf: url)
         }
         
+        /**
+         Evaluates `self` within `context`.
+         
+         - parameter context: The JavaScript context which performs the script.
+         - returns: The resulting `JSValue`, if any.
+         */
         fileprivate func run(in context: JSContext) -> JSValue! {
             return context.evaluateScript(rawValue)
         }
     }
     
+    @discardableResult
     /**
+     When `script` represents a JavaScript file (e.g., _"core.js"_, this will:
+     - locate and load the contents of the file; then
+     - evaluate the script within `self`; then
+     - return the result, if any.
+     
+     - parameter script: The file name of the script.
+     - parameter bundle: The bundle that contains the script. Defaults to `main`.
+     - returns: The result of evaluating the JavaScript, if any.
+     
+     - note: If the file cannot be located within `bundle`, a `LoadError`
+             exception is thrown.
      */
     public func evaluate(script: Script, in bundle: Bundle = .main) throws -> JSValue! {
         return evaluateScript(try script.load(in: bundle))
     }
     
+    @discardableResult
     /**
+     Executes the literal value of `script`.
+     
+     - parameter script: A block of JavaScript code.
+     - returns: The resulting `JSValue`, if any.
      */
     public func execute(script: Script) -> JSValue! {
         return script.run(in: self)
     }
 
     /**
+     The `JSValue` of key.
+     
+     - parameter key: The key for the value.
+     - returns: The value for `key`.
+     
+     - note: A `ValueError` is thrown if value is `null` or `undefined`.
      */
     public func value(_ key: Any!) throws -> JSValue {
         let value: JSValue! = objectForKeyedSubscript(key)
@@ -83,12 +124,20 @@ extension JSContext {
     }
     
     /**
+     Assigns a value to `key`.
+     
+     - parameter key: The label being associated with value.
+     - parameter object: The value.
      */
     public func define<Key: NSCopying & NSObjectProtocol, Value>(_ key: Key, as object: Value!) {
         setObject(object, forKeyedSubscript: key)
     }
     
     /**
+     Assigns a value to `key`.
+     
+     - parameter key: The label being associated with value.
+     - parameter object: The value.
      */
     public func define<T>(_ key: String, as object: T!) {
         define(NSString(string: key), as: object)
